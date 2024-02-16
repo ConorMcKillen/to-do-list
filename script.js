9ec47181646b06b603e8e322b997c311fb6835bb
ConorMcKillen <conorm1@alchemytechs.com>
2024-02-16 20:55:36 +0000

    Updated changes

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 const textInput = document.getElementById("text-input");
-const button = document.getElementById("add-btn");
+const addButton = document.getElementById("add-btn");
+const clearButton = document.getElementById("clear-btn");
 const list = document.getElementById("list");
 
 const items = [];
 
-button.addEventListener("click", addItem);
+addButton.addEventListener("click", addItem);
+clearButton.addEventListener("click", clearList);
 textInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     addItem();
   }
 });
+
+function deleteButton() {
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+}
 
 function addItem() {
   const newItem = textInput.value.trim();
@@ -23,9 +30,23 @@ function addItem() {
 
     // Create a new list item for the latest item
     let li = document.createElement("li");
+    let deleteButton = document.createElement("button");
     li.innerHTML = newItem;
+    deleteButton.innerHTML = `<i class="gg-trash"></i>`;
 
     // Append the new item to the existing list
     list.appendChild(li);
+    li.appendChild(deleteButton);
+
+    deleteButton.addEventListener("click", () => {
+      li.innerHTML = "";
+      list.removeChild(li);
+      items.pop(newItem);
+    });
   }
+}
+
+function clearList() {
+  items.length = 0;
+  list.textContent = "";
 }
